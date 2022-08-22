2e9c35abd931634c1235a3472cb77cb49a973764
somal <Somal1996!github>
2022-08-23 00:48:34 +0300

    Fix

--- arbitrage/exchange_parser/p2p_binance_parser.py
+++ arbitrage/exchange_parser/p2p_binance_parser.py
@@ -22,33 +22,36 @@ BINANCE_REQUEST_HEADER = {
 
 def get_new_p2p_orders():
     print('Get new p2p orders ...')
-    trade_type = 'SELL'
-    for coin in Coin.objects.all():
-        print(f'Handle coin {coin.name}')
-        for payment in Payment.objects.all():
-            print(f'By {payment.name}')
-            param = {
-                'asset': coin.name,
-                'fiat': payment.fiat.name,
-                'page': 1,
-                'payTypes': [payment.name],
-                'rows': 10,
-                'tradeType': trade_type
-            }
-            print(param)
-            respond = requests.post(url=BP2P_URL, headers=BINANCE_REQUEST_HEADER,
-                                    json=param, timeout=10)
-            if respond.status_code == 200:
-                try:
-                    data = respond.json()['data']
-                    if not data is None:
-                        # TODO: make insertion using only 1 request
-                        for order in data:
-                            P2POrder.objects.create(payment=payment, coin=coin,
-                                                    rate=float(order['adv']['price']),
-                                                    author=order['advertiser']['nickName'],
-                                                    type=trade_type, )
-                except Exception as e:
-                    raise ValueError("Error occurred while parsing sellers. Error: ", e)
-            else:
-                raise RuntimeError(f'Cant connect to Binance P2P, status code: {respond.status_code}')
+    for trade_type in P2POrder.TYPES_CHOICES:
+        for coin in Coin.objects.all():
+            print(f'Handle coin {coin.name}')
+            # TODO: check that the system can send 1 request with all possible payments
+            for payment in Payment.objects.all():
+                print(f'By {payment.name}')
+                param = {
+                    'asset': coin.name,
+                    'fiat': payment.fiat.name,
+                    'page': 1,
+                    'payTypes': [payment.name],
+                    'rows': 30,
+                    'tradeType': trade_type[0]
+                }
+                print(param)
+                respond = requests.post(url=BP2P_URL, headers=BINANCE_REQUEST_HEADER,
+                                        json=param, timeout=10)
+                if respond.status_code == 200:
+                    try:
+                        data = respond.json()['data']
+                        if not data is None:
+                            # TODO: make insertion using only 1 request
+                            for order in data:
+                                P2POrder.objects.create(payment=payment, coin=coin,
+                                                        rate=float(order['adv']['price']),
+                                                        author=order['advertiser']['nickName'],
+                                                        type=trade_type, )
+                    except Exception as e:
+                        raise ValueError("Error occurred while parsing sellers. Error: ", e)
+                else:
+                    raise RuntimeError(f'Cant connect to Binance P2P, status code: {respond.status_code}')
+
+# TODO: make deletion of old orders
