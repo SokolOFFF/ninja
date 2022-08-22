522a3da6d38dbd521a13b5224779cc2b35281c9b
somal <Somal1996!github>
2022-08-22 22:34:12 +0300

    Refactor

--- database.py
+++ database.py
@@ -3,23 +3,29 @@ import json
 isRegistration = {}
 isSettingAmount = {}
 
+
 def get_database_json():
     f = open('database.json', 'r')
     data = f.read()
     users = json.loads(data)
     return users
 
+
 def get_users():
     return get_database_json()['users']
+
 
 def get_subscribers():
     return get_database_json()['subscribers']
 
+
 def get_moneyamount(id):
     return get_database_json()['moneyamount'][str(id)]
 
+
 def get_threshold(id):
     return get_database_json()['threshold'][str(id)]
+
 
 def get_all_moneyamount():
     return get_database_json()['moneyamount']
@@ -33,6 +39,7 @@ def set_money_amount(user, mamount):
     f = open("database.json", "w")
     f.write(json.dumps(data))
 
+
 def set_threshold(user, threshold):
     data = get_database_json()
     th = data['threshold']
@@ -40,6 +47,7 @@ def set_threshold(user, threshold):
     data['threshold'] = th
     f = open("database.json", "w")
     f.write(json.dumps(data))
+
 
 def add_user(new_user):
     data = get_database_json()
@@ -49,6 +57,7 @@ def add_user(new_user):
     f = open("database.json", "w")
     f.write(json.dumps(data))
 
+
 def delete_user(user):
     data = get_database_json()
     users = data['users']
@@ -57,6 +66,7 @@ def delete_user(user):
     f = open("database.json", "w")
     f.write(json.dumps(data))
 
+
 def add_subscriber(new_sub):
     data = get_database_json()
     subs = data['subscribers']
@@ -64,6 +74,7 @@ def add_subscriber(new_sub):
     data['subscribers'] = subs
     f = open("database.json", "w")
     f.write(json.dumps(data))
+
 
 def delete_subscriber(sub):
     data = get_database_json()
