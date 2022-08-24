c6e437d2c84be8ba4da101eb387452248a7dbdd2
SokolOFFF <egor1sokolov@gmail.com>
2022-08-24 15:04:24 +0300

    deleted trash

--- arbitrage/exchange_parser/tinkoff_rates_parser.py
+++ arbitrage/exchange_parser/tinkoff_rates_parser.py
@@ -1,5 +1,4 @@
 from tinkoff.invest import Client
-from data.models import Currencies
 
 TINKOFF_TOKEN = 't.6fnOuctj37giwnpnjIYCflRrgzAIJBGtFBO4ttYO43oGCvRC7fnMQ6PPWu6Byk-yGVTKtVAz2BAVNM93tcrFzA'
 
@@ -12,14 +11,3 @@ def get_rate_of(figi='USD000UTSTOM'):
         market_data = client.market_data
         lastprice = convert_price_to_float(market_data.get_last_prices(figi=[figi]).last_prices[0].price)
         return lastprice
-
-def main():
-    # with Client(TINKOFF_TOKEN) as client:
-    #     print(client.instruments.get_favorites().favorite_instruments[0])
-    print(get_rate_of(Currencies.USDRUB_FIGI))
-    print(get_rate_of(Currencies.EURRUB_FIGI))
-    print(get_rate_of(Currencies.GBPRUB_FIGI))
-    print(get_rate_of())
-
-if __name__ == "__main__":
-    main()
